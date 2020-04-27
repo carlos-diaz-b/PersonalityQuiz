8cb6e1deae445e88d4f09a2d67f2cf0084cb9dfa
Carlos Diaz <carlosandresdiazbravo@Carloss-MacBook-Pro.local>
2020-04-27 10:58:35 -0700

    unit3

---
+++ PersonalityQuiz/QuestionData.swift
@@ -0,0 +1,32 @@
+//
+//  QuestionData.swift
+//  PersonalityQuiz
+//
+//  Created by Carlos andres Diaz bravo  on 2020-04-27.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+
+import UIKit
+
+class QuestionData: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+//    struct Question {
+//      var text: String
+//      var type: ResponseType
+//      var answers: [Answer]
+//  }
+//
+//  enum ResponseType {
+//      case single, multiple, ranged
+//  }
+
+
+
+}
